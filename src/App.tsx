29e41752b6f6ad8dddecd494a756cd31f37be3d8
Raymond House <raymondhouse2003@gmail.com>
2024-04-20 12:36:38 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Nameplate} from "./pages/nameplate";
+import {useState} from "react";
+import {SlidingTabBar} from "./Slider";
+import {Experience} from "./pages/Experience";
+import {Projects} from "./pages/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [tabIndex, setTabIndex] = useState<number>(0);
+
+    const tabContents = [
+        <Nameplate />,
+        <Experience />,
+        <Projects />,
+        <div>Skills Content Here</div>
+    ];
+
+        return (
+            <div className="App bg-black grid-background w-screen h-screen">
+                <div className={"flex items-center relative top-16"}>
+                    <SlidingTabBar activeTabIndex={tabIndex} setActiveTabIndex={setTabIndex} />
+                </div>
+                <div className={"flex text-white justify-center items-center relative top-28"}>
+                    {tabContents[tabIndex]}
+                </div>
+            </div>
+        );
+    }
+    export default App;
